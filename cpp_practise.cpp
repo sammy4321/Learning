0bec5337886cfcf40e01b841b995ea2e9273f81f
Samarth M <Samarth.M@Hy-Vee.com>
2025-01-12 09:08:30 +0530

    Added more learning files

---
+++ cpp_practise.cpp
@@ -0,0 +1,6 @@
+// Determine if person's grade based on marks. 
+// Determine if person can vote based on age using ternary operator. 
+#include <iostream>
+int main(){
+    return 0;
+}
--- js_practise.js
+++ js_practise.js
@@ -1,8 +1,2 @@
-const name = "Alice";
-const age = 25;
-
-// Template Literal
-const message = `Hello, my name is ${name} and I am ${age} years old.`;
-
-console.log(message);
-// Output: Hello, my name is Alice and I am 25 years old.
+// Determine if person's grade based on marks. 
+// Determine if person can vote based on age using ternary operator. 
