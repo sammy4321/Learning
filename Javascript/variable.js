1452df2a62fb0595bb4ce97ca4ad44cd3c031cd3
Samarth Gowda <samarth.m.gowda@gmail.com>
2025-02-04 15:28:53 +0530

    Delete Javascript directory

--- Javascript/variable.js
+++
@@ -1,8 +0,0 @@
-const name = "Alice";
-const age = 25;
-
-// Template Literal
-const message = `Hello, my name is ${name} and I am ${age} years old.`;
-
-console.log(message);
-// Output: Hello, my name is Alice and I am 25 years old.
